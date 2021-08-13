3f01fe227e50b0762bd63a08384961b255e58954
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-13 22:08:59 +0300

    just a stub for the Server

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,3 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    static Socket clientSocket;
+
+    static final int S_PORT = 8111;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(S_PORT);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+            clientSocket = serverSocket.accept();
+            System.out.println("New connection accepted");
+
+            final String name = in.readLine();
+
+            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 }
