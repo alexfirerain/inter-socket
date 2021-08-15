f5398d40f079604cfbab14ce8ab38c172e5569d9
Alexander Lukyanov <lux-nova@mail.ru>
2021-08-15 22:26:17 +0300

    [ mutual stub ]

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,20 @@
-public class Client {
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class Client {
+    public static void main(String[] args) {
+        try (
+            Socket clientSocket = new Socket("localhost", Server.S_PORT);
+            BufferedReader inwards = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter outwards = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+            )
+        {
+            outwards.write("Хуй");
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,30 +6,35 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    static Socket clientSocket;
+    static ServerSocket serverSocket;
 
     static final int S_PORT = 8111;
 
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(S_PORT);
+    public static void main(String[] args) throws IOException {
+
+        serverSocket = new ServerSocket(S_PORT);
+
+        try (
+             Socket clientSocket = serverSocket.accept();
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            System.out.println("Establish a new connection accepted from: " +
+                    clientSocket.getRemoteSocketAddress().toString());
 
-            clientSocket = serverSocket.accept();
-            System.out.println("New connection accepted");
+            out.println("Hi, enter your name:");
+            final String clientName = in.readLine();
+            out.println(String.format("Hi %s, you've connected via port #%d",
+                    clientName, clientSocket.getPort()));
 
-            final String name = in.readLine();
-
-            out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            String clientsInput;
+            while ((clientsInput = in.readLine()) != null) {
+                System.out.println("receive: " + clientsInput);
+                int x = Integer.parseInt(clientsInput);
+                out.println(x + 1);
+                System.out.println("send: " + (x + 1));
             }
+
         }
 
     }
